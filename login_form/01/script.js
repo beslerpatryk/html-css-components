6e032f3e908978bc5da7ef783a516798364bd338
Patryk <beslerpatryk@gmail.com>
2021-09-04 17:04:29 +0800

    feat: create login form #01

---
+++ login_form/01/script.js
@@ -0,0 +1,31 @@
+const emailLabel = document.getElementById("email-label")
+const passLabel = document.getElementById("password-label")
+const emailInput = document.getElementById("email")
+const passInput = document.getElementById("password")
+
+console.log(document.activeElement)
+
+var activeElement = document.activeElement 
+
+emailInput.addEventListener('click', higlight)
+passInput.addEventListener('click', higlight)
+
+let active
+
+function higlight(){
+    if(document.activeElement  === emailInput){
+
+        emailLabel.classList.add("active")
+        passLabel.classList.remove("active")
+    }
+    else if(document.activeElement  === passInput){
+        passLabel.classList.add("active")
+        emailLabel.classList.remove("active")
+    }
+    else {
+        emailLabel.classList.remove("active")
+        passLabel.classList.remove("active")
+    }
+
+}
+
